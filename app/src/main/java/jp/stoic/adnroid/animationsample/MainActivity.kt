d213bf5c6d633c3ec54d10760ce9d9d41c745927
Yohei Murayama <muracchi@gmail.com>
2018-08-07 00:35:29 +0900

    fillafterの有無の切り替えを実装

--- app/src/main/java/jp/stoic/adnroid/animationsample/MainActivity.kt
+++ app/src/main/java/jp/stoic/adnroid/animationsample/MainActivity.kt
@@ -8,32 +8,70 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var fadeout = R.anim.fadeout_fillafter
+    private var fadein = R.anim.fadein_fillafter
+    private var slideout = R.anim.slideout_fillafter
+    private var slidein = R.anim.slidein_fillafter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fade.setOnClickListener {
             if (image.visibility == View.VISIBLE) {
-                image.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fadeout))
+                image.startAnimation(AnimationUtils.loadAnimation(this, fadeout))
                 image.visibility = View.GONE
             } else {
-                image.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fadein))
+                image.startAnimation(AnimationUtils.loadAnimation(this, fadein))
                 image.visibility = View.VISIBLE
             }
         }
 
         slide.setOnClickListener {
             if (bar.visibility == View.VISIBLE) {
-                bar.startAnimation(AnimationUtils.loadAnimation(this, R.anim.slideout))
+                bar.startAnimation(AnimationUtils.loadAnimation(this, slideout))
                 bar.visibility = View.GONE
             } else {
-                bar.startAnimation(AnimationUtils.loadAnimation(this, R.anim.slidein))
+                bar.startAnimation(AnimationUtils.loadAnimation(this, slidein))
                 bar.visibility = View.VISIBLE
             }
         }
 
         change.setOnClickListener {
             image.setImageResource(R.drawable.twitter_red)
+        }
+
+        visible.setOnClickListener {
+            image.visibility = View.VISIBLE
+            bar.visibility = View.VISIBLE
+        }
+
+        invisible.setOnClickListener {
+            image.visibility = View.INVISIBLE
+            bar.visibility = View.INVISIBLE
+        }
+
+        gone.setOnClickListener {
+            image.visibility = View.GONE
+            bar.visibility = View.GONE
+        }
+
+        anime_off.setOnClickListener {
+            image.animation = null
+        }
+
+        fa_on.setOnClickListener {
+            fadeout = R.anim.fadeout_fillafter
+            fadein = R.anim.fadein_fillafter
+            slideout = R.anim.slideout_fillafter
+            slidein = R.anim.slidein_fillafter
+        }
+
+        fa_off.setOnClickListener {
+            fadeout = R.anim.fadeout
+            fadein = R.anim.fadein
+            slideout = R.anim.slideout
+            slidein = R.anim.slidein
         }
     }
 }
